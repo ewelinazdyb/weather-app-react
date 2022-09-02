4d3d41d57e9bd0c4d9e78fcbdee01269d3b13aef
ewelinazdyb <evelinazdyb@gmail.com>
2022-09-02 14:37:59 +0100

    Working weather icons for current forecast

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,24 +1,19 @@
 import React from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  solid,
-  regular,
-  brands,
-  icon,
-} from "@fortawesome/fontawesome-svg-core/import.macro";
+import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
 
 export default function WeatherIcon(props) {
   const codeMapping = {
-    "01d": `<FontAwesomeIcon icon={regular('sun')} />`,
-    "02d": `<i className="fa-solid fa-cloud"></i>`,
-    "03d": `<i className="fa-solid fa-cloud"></i>`,
-    "04d": `<i className="fa-solid fa-cloud"></i>`,
-    "09d": `<i className="fa-solid fa-cloud-rain"></i>`,
-    "10d": `<i className="fa-solid fa-cloud-rain"></i>`,
-    "11d": `<i className="fa-solid fa-cloud-bolt"></i>`,
-    "13d": `<i className="fa-solid fa-snowflake"></i>`,
-    "50d": `<i className="fa-solid fa-smog"></i>`,
+    "01d": <FontAwesomeIcon icon={solid("sun")} />,
+    "02d": <FontAwesomeIcon icon={solid("cloud")} />,
+    "03d": <FontAwesomeIcon icon={solid("cloud")} />,
+    "04d": <FontAwesomeIcon icon={solid("cloud")} />,
+    "09d": <FontAwesomeIcon icon={solid("cloud-rain")} />,
+    "10d": <FontAwesomeIcon icon={solid("cloud-rain")} />,
+    "11d": <FontAwesomeIcon icon={solid("cloud-bolt")} />,
+    "13d": <FontAwesomeIcon icon={solid("snowflake")} />,
+    "50d": <FontAwesomeIcon icon={solid("smog")} />,
   };
 
   return <span>{codeMapping[props.icon]}</span>;
