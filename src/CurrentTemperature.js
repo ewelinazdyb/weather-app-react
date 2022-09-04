269f67df02a98c6f7889e5c40fca199824d07beb
ewelinazdyb <evelinazdyb@gmail.com>
2022-09-04 17:53:24 +0100

    Removing unit conversion and fixing spacing

--- src/CurrentTemperature.js
+++ src/CurrentTemperature.js
@@ -1,63 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./Forecast.css";
 
 export default function CurrentTemperature(props) {
-  const [unit, setUnit] = useState("celsius");
-
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  function convertToFahrenheit() {
-    return (props.celsius * 9) / 5 + 32;
-  }
-
-  if (unit === "celsius") {
-    return (
-      <div className="col">
-        <h1>
-          <span className="todays-icon">
-            <WeatherIcon icon={props.icon} />
-          </span>
-          <span className="todays-temp">{Math.round(props.celsius)}</span>
-          <small className="units">°C</small>
-        </h1>
-        <p className="temp-units">
-          <span className="unit">°C</span> |{" "}
-          <span className="unit">
-            <a href="/" onClick={showFahrenheit}>
-              °F
-            </a>
-          </span>
-        </p>
-      </div>
-    );
-  } else {
-    return (
-      <div className="col">
-        <h1>
+  return (
+    <div className="col">
+      <h1>
+        <span className="todays-icon">
           <WeatherIcon icon={props.icon} />
-          <span className="todays-temp">
-            {Math.round(convertToFahrenheit())}
-          </span>
-          <small className="units">°F</small>
-        </h1>
-        <p className="temp-units">
-          <span className="unit">
-            <a href="/" onClick={showCelsius}>
-              °C
-            </a>
-          </span>{" "}
-          | <span className="unit">°F</span>
-        </p>
-      </div>
-    );
-  }
+        </span>
+        <span className="todays-temp">{Math.round(props.celsius)}</span>
+        <small className="units">°C</small>
+      </h1>
+    </div>
+  );
 }
--- src/WeeklyForecast.js
+++ src/WeeklyForecast.js
@@ -1,18 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./WeeklyForecast.css";
 import axios from "axios";
 import WeeklyForecastDay from "./WeeklyForecastDay";
 
 export default function WeeklyForecast(props) {
-  const [ready, setReady] = useState(false);
+  const [loaded, setLoaded] = useState(false);
   const [forecastData, setForecastData] = useState(null);
 
   function handleResponse(response) {
     setForecastData(response.data.daily);
-    setReady(true);
+    setLoaded(true);
   }
 
-  if (ready) {
+  function load() {
+    let lat = props.coord.lat;
+    let lon = props.coord.lon;
+    const apiKey = `07c8d029e683ec94d2784e3188d6f11d`;
+    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coord]);
+
+  if (loaded) {
     return (
       <div className="weekly-forecast">
         <div class="row">
@@ -23,18 +36,15 @@ export default function WeeklyForecast(props) {
                   <WeeklyForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
       </div>
     );
   } else {
-    let lat = props.coord.lat;
-    let lon = props.coord.lon;
-    const apiKey = `07c8d029e683ec94d2784e3188d6f11d`;
-    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(handleResponse);
+    load();
 
     return null;
   }
